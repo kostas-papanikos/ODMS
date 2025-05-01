a271fbacd1f50f8284653ed46f9aa0d46a4900a4
Sotiria Mountzouri <t8230099@aueb.gr>
2025-05-01 23:35:25 +0300

    js for menu

---
+++ js/Menu.js
@@ -0,0 +1,25 @@
+const data = {
+  "order1": `
+  <ul>
+    <li>Τρέχουσα Τοποθεσία: Λιμάνι Σιγκαπούρης</li>
+    <li>Αναμενόμενη παράδοση: 5-7 εργάσιμες μέρες</li>
+    <li>Προειδοποίηση για Καθυστερήσεις: Όχι</li>
+  </ul>
+  <p><a href="invoices/timologio_AB12345.pdf" target="_blank" class="invoice-link">📄 Τιμολόγιο Παραγγελίας (PDF)</a></p>
+    `
+  };
+  
+  document.getElementById("search").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const code = document.getElementById("code").value.trim();
+    const results = document.getElementById("results");
+  
+    if (data[code]) {
+      results.innerHTML = data[code];
+    } else {
+      results.textContent = "";
+      alert("Δεν βρέθηκε κωδικός παραγγελίας");
+    }
+  });
+  
